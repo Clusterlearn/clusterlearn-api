c181dab47d83364a452e551595e3f52c6652ef82
Aniezeofor Chibueze Michael <aniezeoformic@gmail.com>
2023-10-28 02:19:48 +0000

    try fix

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,56 @@
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
 import express, { Router } from 'express';
 import helmet from 'helmet';
 import hpp from 'hpp';
 import bodyParser from 'body-parser';
 import compression from 'compression';
 import { config } from 'dotenv';
-config({path: `.env.${process.env.NODE_ENV || 'development'}.local` })
-// import {logger } from './utils/logger'
 import mongoose from 'mongoose';
 import redisClient from './services/redis';
 
+// Import the CORS library
+import cors from 'cors';
 
 class App {
     app 
     port : string | number = 3000
     env : string = "development"
+    
     constructor() {
         this.app = express();
         this.env = process.env.NODE_ENV || 'development';
-        this.initaliseMiddleware()
-        this.startMongoose()
+        this.initaliseMiddleware();
+        this.startMongoose();
+        this.setupCORS(); // Call the setupCORS method
     }
 
-    private initaliseMiddleware()
-    {
+    private initaliseMiddleware() {
+        // a middleware to console log the each request
+        this.app.use((req, res, next) => {
+            console.log(`New request received at ${Date.now()} , for ${req.url} and method ${req.method}`);
+            next();
+        });
         this.app.use(cookieParser());
-        this.app.use(compression())
-        this.app.use(cors());
+        this.app.use(compression());
         this.app.use(helmet());
         this.app.use(hpp());
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: true }));
     }
 
-    public useRoute(path :string , Route : Router){
-        this.app.use(path, Route)
-        return this
-    }   
+    // Setup the CORS configuration
+    private setupCORS() {
+        const corsOptions = {
+            origin: '*', // Allow requests from any origin
+            methods: 'GET,POST,PUT,DELETE,OPTIONS' // Allow these verbs
+        };
+        this.app.use(cors(corsOptions));
+    }
 
+    public useRoute(path :string , Route : Router) {
+        this.app.use(path, Route);
+        return this;
+    }   
 
     public start(port ?: string|number) {
         this.app.listen(port ?? this.port, () => {
@@ -48,11 +60,10 @@ class App {
             console.info(`=================================`);
         });
     }
-    
-    private startMongoose()
-    {
-        const { MONGODB_USER, MONGODB_PASS, MONGODB_HOST, MONGODB_DB, MONGODB_URL } = process.env
-        console.log(MONGODB_URL)
+
+    private startMongoose() {
+        const { MONGODB_USER, MONGODB_PASS, MONGODB_HOST, MONGODB_DB, MONGODB_URL } = process.env;
+        console.log(MONGODB_URL);
         mongoose.connect(MONGODB_URL ?? `mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@${MONGODB_HOST}/${MONGODB_DB}?retryWrites=true&w=majority`)
         .then(() => {
             console.log('🚀 Connected to MongoDB');
@@ -62,8 +73,7 @@ class App {
         });
     }
 
-    closeMongoose()
-    {
+    closeMongoose() {
         process.on('SIGINT', async () => {
             try {
                 await mongoose.disconnect();
@@ -74,11 +84,10 @@ class App {
                 process.exit(1);
             }
         });
-        return this
+        return this;
     }
 
-    closeRedis()
-    {
+    closeRedis() {
         redisClient.on('end', () => {
             console.log('❌ Disconnected from Redis');
         });
@@ -87,8 +96,7 @@ class App {
             console.error('❌ Error connecting to Redis Cloud:', error);
             redisClient.disconnect(); // Disconnect from Redis on error
         });
-        return this
-
+        return this;
     }
 }
 
