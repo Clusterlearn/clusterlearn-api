1bb3c5516475a8a292393b8b346758eafea5a6fc
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2023-11-10 22:28:58 +0100

    added google meet generation and sending stuff

--- src/controllers/meeting.ts
+++ src/controllers/meeting.ts
@@ -1,5 +1,22 @@
+import { generateMeetingLink } from "@/services/google";
+import sendMessage from "@/services/mailer";
 import { groupMember, group_member } from "@/types/modelData";
 
 export async function scheduleMeeting<T extends group_member>(members: groupMember<T>) {
-    const mails = members.map(member => member.email)
+    try {
+        const data = await generateMeetingLink(members.map(member => member.email))
+        if(!data.status) throw new Error(data.message) 
+        const mails = members.forEach(member => {
+            sendMessage({
+                from: process.env.MAIL_ADDRESS,
+                to: member.email,
+                subject: 'Your cluster is ready',
+                text: `Your cluster is ready, join the meeting at ${data.link}`,
+                html: `<p>Your cluster is ready, join the meeting at <a href="${data.link}">${data.link}</a></p>`
+            })
+        })
+    }
+    catch (e) {
+        console.log(e)
+    }
 }
