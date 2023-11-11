622ecdd0e2b1d7294175519135989587501218e7
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2023-11-11 08:55:17 +0100

    tix

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -11,7 +11,7 @@ router.use(async function (req, res, next) {
     // get ip address
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || [];
     console.log(`Admmin request from ${ip} on ${req.url} at ${new Date().toLocaleString()}`);
-    if(await redisClient.get(`recent:${ip[0]}`)) next();
+    if(await redisClient.get(`recent:${ip[0]}`)) return next();
     // check header or url parameters or post parameters for token
     const token = req.headers['authorization'] ?? req.query.admintoken ?? req.body.admintoken;
     if (token !== process.env.ADMIN_TOKEN) return res.status(401).send(responseHandler.errorJson({
@@ -20,7 +20,7 @@ router.use(async function (req, res, next) {
 
     // set to redis for 30 minutes
     await redisClient.set(`recent:${ip[0]}`, 1, 'EX', 60 * 30);
-    next();
+    return next();
 });
 
 router.get('/login', async (req, res) => {
