8cfdabe581e9269c6415f757484ebc65a390a893
Aniezeofor Chibueze Michael <aniezeoformic@gmail.com>
2023-12-08 21:57:54 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -59,7 +59,7 @@ class App {
             res.setHeader('Access-Control-Allow-Origin', '*');
             res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
             res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-            res.setHeader('Access-Control-Allow-Credentials', true);
+            res.setHeader('Access-Control-Allow-Credentials', "true");
             next();
         });
     }
